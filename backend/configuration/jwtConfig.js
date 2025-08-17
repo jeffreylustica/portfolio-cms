97a65a61c7342a94d13fae04a32b6c112e882d1c
jeff <jefflustica2@gmail.com>
2025-08-17 17:26:29 +0800

    chore: update secret key

--- backend/configuration/jwtConfig.js
+++ backend/configuration/jwtConfig.js
@@ -1,6 +1,9 @@
 import crypto from "crypto";
+import dotenv from "dotenv";
+dotenv.config();
 
 // generate random secret key
-const secretKey = crypto.randomBytes(32).toString("hex");
+// const secretKey = crypto.randomBytes(32).toString("hex");
+const secretKey = process.env.JWT_SECRET_KEY;
 
 export default secretKey;
